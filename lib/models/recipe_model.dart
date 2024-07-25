8e10e6429d7039d72fed62c8a78e494c77339f9b
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-25 21:31:13 +0545

    Recipe Model Modified

--- lib/models/recipe_model.dart
+++ lib/models/recipe_model.dart
@@ -3,18 +3,41 @@ class RecipeModel {
   late String appImgUrl;
   late double appCalories;
   late String appUrl;
+  late List<Ingredient> ingredients;
 
   RecipeModel({
     this.appLabel = "LABEL",
     this.appCalories = 0.000,
     this.appImgUrl = "IMAGE",
     this.appUrl = "URL",
+    this.ingredients = const [],
   });
+
   factory RecipeModel.fromMap(Map recipe) {
+    List<Ingredient> ingredientsList = (recipe["ingredients"] as List)
+        .map((ingredient) => Ingredient.fromMap(ingredient))
+        .toList();
+
     return RecipeModel(
-        appLabel: recipe["label"],
-        appCalories: recipe["calories"],
-        appImgUrl: recipe["image"],
-        appUrl: recipe["url"]);
+      appLabel: recipe["label"],
+      appCalories: recipe["calories"],
+      appImgUrl: recipe["image"],
+      appUrl: recipe["url"],
+      ingredients: ingredientsList,
+    );
+  }
+}
+
+class Ingredient {
+  final String text;
+  final double weight;
+
+  Ingredient({required this.text, required this.weight});
+
+  factory Ingredient.fromMap(Map<String, dynamic> map) {
+    return Ingredient(
+      text: map['text'],
+      weight: map['weight'].toDouble(),
+    );
   }
 }
